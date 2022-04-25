d9988003574c6f7bba408de0736989f9f803ba04
Sallai András <andrewteki@gmail.com>
2022-04-25 20:10:52 +0200

    Kezdés

---
+++ src/MainWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+
+        this.setTitle("App");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(400, 300);
+        this.setVisible(true);
+    }
+    
+}
